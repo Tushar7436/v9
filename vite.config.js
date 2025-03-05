0fb1a31cbc26232dba9984a647117e095a43ed78
Tushar Agarwal <tushar7436@gmail.com>
2025-03-05 23:26:13 +0530

    v9_updates

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,16 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0].toString(); // Split by package
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Set to a higher value if needed
+  },
 });
